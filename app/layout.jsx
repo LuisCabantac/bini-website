03b44579e43ff9878755eaeb3c387c97afbdf24f
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-09-16 21:34:50 +0800

    Remove 100vh on both the layout and the page causing a space at the bottom of the main page on mobile screens

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${worksans.className} min-h-screen bg-[#ace4ea]`}>
+      <body className={`${worksans.className} bg-[#ace4ea]`}>
         <NavProvider>
           <Nav />
         </NavProvider>
