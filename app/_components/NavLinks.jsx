f5c3be2f5dc259852b424f8b423c69710d5e77a0
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-09-17 14:04:02 +0800

    Refactor nav icon's conditional rendering for mobile

--- app/_components/NavLinks.jsx
+++ app/_components/NavLinks.jsx
@@ -18,37 +18,24 @@ export default function NavLinks() {
     <div className="z-10 relative md:bg-transparent">
       <div className="md:hidden flex absolute top-[-0.6rem] right-[-0.3rem] z-30">
         <button onClick={handleSetIsMobileNav}>
-          {isMobileNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18 18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 9h16.5m-16.5 6.75h16.5"
-              />
-            </svg>
-          )}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d={`${
+                isMobileNav
+                  ? "M6 18 18 6M6 6l12 12"
+                  : "M3.75 9h16.5m-16.5 6.75h16.5"
+              }`}
+            />
+          </svg>
         </button>
       </div>
 
