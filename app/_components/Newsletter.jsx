1a61c14eb661033cf905440147bc1646cb327d76
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-09-19 06:57:41 +0800

    Remove email icon from the newsletter bar

--- app/_components/Newsletter.jsx
+++ app/_components/Newsletter.jsx
@@ -35,25 +35,10 @@ export default function Newsletter() {
             className="flex flex-1 justify-between items-center bg-[#eefafb] rounded-full"
             onSubmit={handleSetEmail}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-9 pl-4 stroke-[#99beb3]"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
-              />
-            </svg>
-
             <input
               type="email"
               placeholder="Enter your email"
-              className="bg-[#eefafb] px-2 md:py-0 py-2 focus:outline-0 grow rounded-full placeholder:text-[#99beb3] outline-none"
+              className="bg-[#eefafb] px-6 md:py-0 py-2 focus:outline-0 grow rounded-full placeholder:text-[#99beb3] outline-none"
               value={email}
               onChange={handleSetEmail}
             />
