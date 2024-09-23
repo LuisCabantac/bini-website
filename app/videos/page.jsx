509be316e06991f37d171249a44dd4686e881018
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-09-23 12:46:29 +0800

    Revert "Implement try and catch for the videos section"
    This reverts commit b7dd26dbbc90cd805a7f4d56c53f8c867626e41f.

--- app/videos/page.jsx
+++ app/videos/page.jsx
@@ -9,22 +9,17 @@ export const metadata = {
 };
 
 export default async function Page() {
-  try {
-    const [musicVideos, latestVideos, playlistVideos] = await Promise.all([
-      getMusicVideos(),
-      getVideos("", 6, "video"),
-      getPlaylists(6),
-    ]);
+  const [musicVideos, latestVideos, playlistVideos] = await Promise.all([
+    getMusicVideos(),
+    getVideos("", 6, "video"),
+    getPlaylists(6),
+  ]);
 
-    return (
-      <div className="md:mt-14 mt-12 md:mx-16 mx-6 md:mb-20 mb-12 flex flex-col md:gap-16 gap-12">
-        <MusicVideos musicVideos={musicVideos} />
-        <LatestVideos videos={latestVideos} />
-        <Playlist videos={playlistVideos} />
-      </div>
-    );
-  } catch (err) {
-    console.error(err.message);
-    return <p className="font-semibold md:text-2xl text-xl">{err.message}</p>;
-  }
+  return (
+    <div className="md:mt-14 mt-12 md:mx-16 mx-6 md:mb-20 mb-12 flex flex-col md:gap-16 gap-12">
+      <MusicVideos musicVideos={musicVideos} />
+      <LatestVideos videos={latestVideos} />
+      <Playlist videos={playlistVideos} />
+    </div>
+  );
 }
