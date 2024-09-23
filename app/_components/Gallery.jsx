19e2c7ec233b3ce44b9abc8483f1b6f46260518b
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-09-23 10:38:12 +0800

    Redesign the Gallery section

--- app/_components/Gallery.jsx
+++ app/_components/Gallery.jsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
-import HeaderTitle from "@/app/_components/HeaderTitle";
 import galleryData from "@/app/_lib/gallery-data";
+
+import HeaderTitle from "@/app/_components/HeaderTitle";
+import GalleryCards from "@/app/_components/GalleryCards";
 
 export default function Gallery() {
   const [zoomedImage, setZoomedImage] = useState(null);
@@ -21,18 +22,32 @@ export default function Gallery() {
   return (
     <section className="md:mx-16 mx-6">
       <HeaderTitle type="h2">Gallery</HeaderTitle>
-      <div className="grid md:grid-cols-[auto_auto_auto_auto] grid-cols-[auto_auto] grid-flow-row rounded-lg overflow-hidden md:mt-6 mt-4">
-        {galleryData.map((item) => (
-          <div
-            key={item.id}
-            onClick={() => openZoomedImage(item.url)}
-            className="md:w-full md:h-[26rem] w-full h-[18rem]"
-          >
-            <div className="relative w-full h-full">
-              <Image src={item.url} alt="image" fill className="object-cover" />
-            </div>
-          </div>
-        ))}
+      <div className="grid grid-cols-2 md:grid-cols-4 md:gap-4 gap-2 md:mt-6 mt-4">
+        <GalleryCards
+          images={galleryData.slice(0, 4)}
+          padding=""
+          height="md:h-[26rem]"
+          openZoomedImage={openZoomedImage}
+        />
+        <GalleryCards
+          images={galleryData.slice(4, 8)}
+          padding="md:pt-4 pt-0"
+          height="md:h-[26rem]"
+          openZoomedImage={openZoomedImage}
+        />
+        <GalleryCards
+          images={galleryData.slice(8, 12)}
+          padding=""
+          height="md:h-[26rem]"
+          openZoomedImage={openZoomedImage}
+        />
+        <GalleryCards
+          images={galleryData.slice(12, 16)}
+          padding="md:pt-4 pt-0"
+          height="md:h-[26rem]"
+          openZoomedImage={openZoomedImage}
+        />
+
         {zoomedImage && (
           <div className="zoomed__image__container" onClick={closeZoomedImage}>
             <div className="md:w-[30%] md:h-[60%] w-[80%] h-[40%] flex justify-center items-center">
---
+++ app/_components/GalleryCards.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function GalleryCards({
+  images,
+  padding,
+  height,
+  openZoomedImage,
+}) {
+  return (
+    <div className={`flex flex-col md:gap-4 gap-2 ${padding}`}>
+      {images.map((item) => (
+        <div
+          key={item.id}
+          onClick={() => openZoomedImage(item.url)}
+          className={`md:w-full ${height} w-full h-[18rem]`}
+        >
+          <div className="relative w-full h-full">
+            <Image
+              src={item.url}
+              alt="image"
+              fill
+              className="object-cover rounded-lg"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
